5d01ae3ddf1c73bf4058d675cf070999c23faf3d
Grzegorz Wolfinger <grzegorz.wolfinger@gmail.com>
2023-07-13 14:22:18 +0200

    Update auth.js

--- util/auth.js
+++ util/auth.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const BASE_URL = 'https://identitytoolkit.googleapis.com/v1/accounts:'
-const API_KEY = 'AIzaSyBPcwoKz0aPog1X-mnKxbr255C0d1PcM84'
+
 
 export const authenticate = async (mode, email, password) => {
   const response = await axios.post(`${BASE_URL}${mode}?key=${API_KEY}`, {
